8246a1fb089dfc26cdd3a62688685d2e01721779
Ryan Davies <ryan@ryandavies.net>
2012-10-28 19:56:26 +0000

    Added a test interface.

---
+++ Specs/LBTestViewController.h
@@ -0,0 +1,13 @@
+//
+//  LBTestViewController.h
+//  Braille
+//
+//  Created by Ryan Davies on 28/10/2012.
+//  Copyright (c) 2012 Ryan Davies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LBTestViewController : UIViewController
+
+@end
---
+++ Specs/LBTestViewController.m
@@ -0,0 +1,13 @@
+//
+//  LBTestViewController.m
+//  Braille
+//
+//  Created by Ryan Davies on 28/10/2012.
+//  Copyright (c) 2012 Ryan Davies. All rights reserved.
+//
+
+#import "LBTestViewController.h"
+
+@interface LBTestViewController ()
+
+@end
